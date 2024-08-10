4cae3175611f70298b04a6025f78e29381cdace2
rajendra-s-patil <rajendra.shrinivas.patil@gmail.com>
2024-08-10 15:29:30 +0530

    added message box

--- WhiteBooks/LOGIN_SCREEN.vb
+++ WhiteBooks/LOGIN_SCREEN.vb
@@ -1,6 +1,7 @@
 ﻿Public Class LOGIN_SCREEN
     Private Sub OK_BUTTON_Click(sender As Object, e As EventArgs) Handles OK_BUTTON.Click
-        MAIN_SCREEN.Show()
+        MsgBox("hello world")
+        'MAIN_SCREEN.Show()
     End Sub
 
     Private Sub CANCEL_Click(sender As Object, e As EventArgs) Handles CANCEL.Click
